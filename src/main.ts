f0a8161da636b1994a783d74d25e32b38d4b91d0
DaiLocIU <62388836+DaiLocIU@users.noreply.github.com>
2020-08-18 13:05:36 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -48,7 +48,7 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
   app.useGlobalFilters(new HttpExceptionFilter());
 
-  await app.listen(appConfig.port,'0,0,0,0', () => {
+  await app.listen(appConfig.port, () => {
     logger.log('Listening at ' + appConfig.domain + '/');
   });
 }
